c3848de929af3a7f046c484af1928e79940f6273
felipenedel <felipenedel@gmail.com>
2025-04-10 23:03:26 -0300

    Attribute roller

---
+++ app/char-designer/components/attribute_roller.tsx
@@ -0,0 +1,108 @@
+import {HStack, IconButton, Table, Text, VStack} from "@chakra-ui/react";
+import {CopyX, Dice1, Dice2, Dice3, Dice4, Dice5, Dice6, Shuffle} from "lucide-react";
+import React, {useState} from "react";
+
+export default function AttributeRoller() {
+  const items = [
+    {id: 1, name: "Strength", rolls: [], modifier: 0, fixed: false},
+    {id: 2, name: "Dexterity", rolls: [], modifier: 0, fixed: false},
+    {id: 3, name: "Constitution", rolls: [], modifier: 0, fixed: false},
+    {id: 4, name: "Intelligence", rolls: [], modifier: 0, fixed: false},
+    {id: 5, name: "Wisdom", rolls: [], modifier: 0, fixed: false},
+    {id: 6, name: "Charisma", rolls: [], modifier: 0, fixed: false}
+  ]
+
+  const [rolledItems, setRolledItems] = useState<Item[]>(items);
+
+  const calculateModifier = (roll: number): number => {
+    if (!roll) return 0;
+    if (roll <= 3) return -2;
+    if (roll <= 7) return -1;
+    if (roll <= 13) return 0;
+    if (roll <= 17) return 1;
+    return 2;
+  }
+
+  type Item = {
+    id: number;
+    name: string;
+    rolls: number[];
+    modifier: number;
+    fixed: boolean;
+  };
+
+  const rollAttributes = () => {
+    const rolledItems = items.map((item) => {
+      const roll1 = Math.floor(Math.random() * 6 + 1)
+      const roll2 = Math.floor(Math.random() * 6 + 1)
+      const roll3 = Math.floor(Math.random() * 6 + 1)
+
+      return {...item, rolls: [roll1, roll2, roll3]};
+    });
+
+    setRolledItems(rolledItems);
+  }
+
+  const sumRolls = (item) => {
+    const sum = item.rolls?.reduce((total: number, roll) => {
+      return total + roll;
+    }, 0);
+
+    return sum;
+  }
+
+  const fixItemTo14 = (itemId: number) => {
+    const updatedItems = rolledItems.map((item) => {
+      return {...item, fixed: item.id === itemId}
+    });
+
+    setRolledItems(updatedItems);
+  }
+
+  const diceComponents = [Dice1, Dice2, Dice3, Dice4, Dice5, Dice6];
+
+  return (
+    <VStack>
+      <Table.Root size="sm" variant={"line"}>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeader>Attribute</Table.ColumnHeader>
+            <Table.ColumnHeader>Sum</Table.ColumnHeader>
+            <Table.ColumnHeader>Modifier</Table.ColumnHeader>
+            <Table.ColumnHeader>Set to 14</Table.ColumnHeader>
+            <Table.ColumnHeader>Rolls</Table.ColumnHeader>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {rolledItems.map((item) => (
+            <Table.Row key={item.id}>
+              <Table.Cell>{item.name}</Table.Cell>
+              <Table.Cell>{(item.fixed ? "14 (" + sumRolls(item) + ")" : sumRolls(item)) || "-"}</Table.Cell>
+              <Table.Cell>{(item.fixed ? `${calculateModifier(14)} (${calculateModifier(sumRolls(item))})` : sumRolls(item) ? calculateModifier(sumRolls(item)) : "-")}</Table.Cell>
+              <Table.Cell>
+                <IconButton disabled={rolledItems.some((item) => item.rolls.length === 0)} onClick={() => fixItemTo14(item.id)} size="sm" variant="ghost">
+                  <CopyX/>
+                </IconButton>
+              </Table.Cell>
+              <Table.Cell>
+                {
+                  <HStack>
+                    {item.rolls?.map((id, index) => {
+                      const DiceComponent = diceComponents[id - 1];
+                      return <DiceComponent key={index}/>;
+                    })}
+                  </HStack>
+                }
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+      <IconButton onClick={rollAttributes}>
+        <Shuffle/>
+        <Text>Roll</Text>
+      </IconButton>
+    </VStack>
+  )
+
+}
--- app/char-designer/page.tsx
+++ app/char-designer/page.tsx
@@ -1,18 +1,24 @@
 "use client";
 
-import {Card, Heading, HStack, IconButton, Input, RadioGroup, Stack, Text, VStack} from "@chakra-ui/react";
+import {Card, Collapsible, Heading, HStack, IconButton, Input, RadioGroup, Stack, Text, VStack} from "@chakra-ui/react";
 import {Shuffle} from "lucide-react";
 import {GeneratedName} from "@/types/name-generator/generated_name_type";
 import {useState} from "react";
+import AttributeRoller from "@/app/char-designer/components/attribute_roller";
 
 export default function Page() {
   const [name, setName] = useState<string>("");
   const [goal, setGoal] = useState<string>("");
   const [gender, setGender] = useState<string>("");
+  const [assignType, setAssignType] = useState<string>("");
   const genderOptions = [
     {label: "Male", value: "m"},
     {label: "Female", value: "f"},
     {label: "Undefined or irrelevant", value: "-"},
+  ]
+  const assignOptions = [
+    {label: "Roll 3d6", value: "roll"},
+    {label: "Assign", value: "assign"}
   ]
 
   const randomizeName = () => {
@@ -27,42 +33,72 @@ export default function Page() {
     }
   };
 
-  const labelStyles = { display: "inline-block", minW: "80px", flexShrink: 0 };
+  const labelStyles = {display: "inline-block", minW: "80px", flexShrink: 0};
 
   return (
     <Stack>
       <Heading size="4xl">Char Designer</Heading>
-      <Card.Root size="sm">
-        <Card.Header>
-          <Heading size="md">Char Details</Heading>
-        </Card.Header>
-        <Card.Body color="fg.muted">
-          <VStack align="start" gap={4}>
-            <RadioGroup.Root value={gender} onValueChange={(e) => setGender(e.value || "")}>
-              <HStack gap="6">
-                {genderOptions.map((item) => (
-                  <RadioGroup.Item key={item.value} value={item.value}>
-                    <RadioGroup.ItemHiddenInput/>
-                    <RadioGroup.ItemIndicator/>
-                    <RadioGroup.ItemText>{item.label}</RadioGroup.ItemText>
-                  </RadioGroup.Item>
-                ))}
-              </HStack>
-            </RadioGroup.Root>
-            <HStack w="full">
-              <Text {...labelStyles}>Name</Text>
-              <Input value={name} onChange={(e) => setName(e.target.value)}/>
-              <IconButton onClick={randomizeName}>
-                <Shuffle/>
-              </IconButton>
-            </HStack>
-            <HStack  w="full">
-              <Text {...labelStyles}>Goal</Text>
-              <Input value={goal} onChange={(e) => setGoal(e.target.value)}/>
-            </HStack>
-          </VStack>
-        </Card.Body>
-      </Card.Root>
+      <Collapsible.Root>
+        <Collapsible.Trigger paddingY="3">Toggle Collapsible</Collapsible.Trigger>
+        <Collapsible.Content>
+          <Card.Root size="sm">
+            <Card.Header>
+              <Heading size="md">Char Details</Heading>
+            </Card.Header>
+            <Card.Body color="fg.muted">
+              <VStack align="start" gap={4}>
+                <RadioGroup.Root value={gender} onValueChange={(e) => setGender(e.value || "")}>
+                  <HStack gap="6">
+                    {genderOptions.map((item) => (
+                      <RadioGroup.Item key={item.value} value={item.value}>
+                        <RadioGroup.ItemHiddenInput/>
+                        <RadioGroup.ItemIndicator/>
+                        <RadioGroup.ItemText>{item.label}</RadioGroup.ItemText>
+                      </RadioGroup.Item>
+                    ))}
+                  </HStack>
+                </RadioGroup.Root>
+                <HStack w="full">
+                  <Text {...labelStyles}>Name</Text>
+                  <Input value={name} onChange={(e) => setName(e.target.value)}/>
+                  <IconButton onClick={randomizeName}>
+                    <Shuffle/>
+                  </IconButton>
+                </HStack>
+                <HStack w="full">
+                  <Text {...labelStyles}>Goal</Text>
+                  <Input value={goal} onChange={(e) => setGoal(e.target.value)}/>
+                </HStack>
+                <Text>Roll 3d6 and set one to 14 or assign based on a predefined array</Text>
+                <RadioGroup.Root value={assignType} onValueChange={(e) => setAssignType(e.value || "")}>
+                  <HStack gap="6">
+                    {assignOptions.map((item) => (
+                      <RadioGroup.Item key={item.value} value={item.value}>
+                        <RadioGroup.ItemHiddenInput/>
+                        <RadioGroup.ItemIndicator/>
+                        <RadioGroup.ItemText>{item.label}</RadioGroup.ItemText>
+                      </RadioGroup.Item>
+                    ))}
+                  </HStack>
+                </RadioGroup.Root>
+                {
+                  assignType === "assign" && (
+                    <HStack w="full">
+                      <Text {...labelStyles}>Roll</Text>
+                      <Input value={name} onChange={(e) => setName(e.target.value)}/>
+                    </HStack>
+                  )
+                }
+                {
+                  assignType === "roll" && (
+                    <AttributeRoller/>
+                  )
+                }
+              </VStack>
+            </Card.Body>
+          </Card.Root>
+        </Collapsible.Content>
+      </Collapsible.Root>
     </Stack>
   )
 }
