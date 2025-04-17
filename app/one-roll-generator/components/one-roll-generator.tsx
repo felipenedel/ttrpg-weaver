f413d62f3d1b8d4a20b3afcda68064727f2aa0c9
felipenedel <felipenedel@gmail.com>
2025-04-17 20:42:34 -0300

    Simplifying

--- app/one-roll-generator/components/one-roll-generator.tsx
+++ app/one-roll-generator/components/one-roll-generator.tsx
@@ -2,48 +2,7 @@
 
 import {Box, Button, DataList, StackSeparator, VStack} from "@chakra-ui/react";
 import React, {useState} from "react";
-
-const labelMappings = {
-  npc: [
-    {key: "age", label: "Age"},
-    {key: "background", label: "Background"},
-    {key: "role", label: "Role"},
-    {key: "biggestProblem", label: "Biggest problem"},
-    {key: "greatestDesire", label: "Greatest desire"},
-    {key: "mainTrait", label: "Main trait"}
-  ],
-  patron: [
-    {key: "eagernessToHire", label: "Eagerness to hire"},
-    {key: "trustworthiness", label: "Trustworthiness"},
-    {key: "challenge", label: "Challenge"},
-    {key: "countervailingForce", label: "Countervailing force"},
-    {key: "potentialRewards", label: "Potential rewards"},
-    {key: "complications", label: "Complications"}
-  ],
-  urban_encounter: [
-    {key: "conflict", label: "Conflict"},
-    {key: "venue", label: "Venue"},
-    {key: "whyPcInvolved", label: "Why is PC involved"},
-    {key: "eventNature", label: "Event nature"},
-    {key: "antagonists", label: "Antagonists"},
-    {key: "relevantFeatures", label: "Relevant features"}
-  ],
-  wilderness_encounter: [
-    {key: "range", label: "Range"},
-    {key: "weatherAndLighting", label: "Weather and lighting"},
-    {key: "encounterNature", label: "Encounter nature"},
-    {key: "friendlyCreatures", label: "Friendly creatures"},
-    {key: "hostileCreatures", label: "Hostile creatures"},
-    {key: "relevantFeaturesNearby", label: "Relevant features nearby"}
-  ]
-};
-
-const buttonTextMappings = {
-  npc: "Generate NPC",
-  patron: "Generate Patron",
-  urban_encounter: "Generate Urban Encounter",
-  wilderness_encounter: "Generate Wilderness Encounter"
-};
+import {buttonTextMappings, labelMappings} from "@/app/one-roll-generator/components/label-mappings";
 
 interface OneRollGeneratorProps {
   type: "npc" | "patron" | "urban_encounter" | "wilderness_encounter";
