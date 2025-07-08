f6cdc116e028f74265b5c48cb7a5d56c124f6ebf
felipenedel <felipenedel@gmail.com>
2025-07-08 09:00:05 -0300

    WIP

--- app/char-designer/components/char_details.tsx
+++ app/char-designer/components/char_details.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import {HStack, IconButton, Input, RadioGroup, Text, VStack} from "@chakra-ui/react";
+import {HStack, IconButton, Input, Popover, Portal, RadioGroup, Text, VStack} from "@chakra-ui/react";
 import {Shuffle} from "lucide-react";
 import {GeneratedName} from "@/types/name-generator/generated_name_type";
 import React, {useState} from "react";
 
 export default function CharDetails() {
   const [name, setName] = useState<string>("");
+  const [generatedName, setGeneratedName] = useState<GeneratedName | null>(null);
+  const [openNameGenerator, setOpenNameGenerator] = useState(false)
   const [goal, setGoal] = useState<string>("");
   const [gender, setGender] = useState<string>("");
   const genderOptions = [
@@ -20,7 +22,7 @@ export default function CharDetails() {
       fetch(`/api/name-generator`)
         .then((res) => res.json() as Promise<GeneratedName>)
         .then((data) => {
-          setName(data.nameMale);
+          setGeneratedName(data);
         });
     } catch (err) {
       console.error("Error fetching API:", err);
@@ -48,9 +50,23 @@ export default function CharDetails() {
       <HStack w="full">
         <Text {...labelStyles}>Name</Text>
         <Input value={name} onChange={(e) => setName(e.target.value)}/>
-        <IconButton onClick={randomizeName}>
-          <Shuffle/>
-        </IconButton>
+        <Popover.Root open={openNameGenerator} onOpenChange={(e) => setOpenNameGenerator(e.open)}>
+          <Popover.Trigger asChild>
+            <IconButton onClick={randomizeName}>
+              <Shuffle/>
+            </IconButton>
+          </Popover.Trigger>
+          <Portal>
+            <Popover.Positioner>
+              <Popover.Content>
+                <Popover.Arrow/>
+                <Popover.Body>
+                  This is a popover with the same width as the trigger button
+                </Popover.Body>
+              </Popover.Content>
+            </Popover.Positioner>
+          </Portal>
+        </Popover.Root>
       </HStack>
       <HStack w="full">
         <Text {...labelStyles}>Goal</Text>
